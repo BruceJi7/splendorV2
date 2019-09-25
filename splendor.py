ccc833ea8c1a8aff87d4b35aa55f62eec2240315
Tobias <revolverc24@gmail.com>
2019-09-25 18:40:14 +0900

    add table tokens, add table to purchas function

--- splendor.py
+++ splendor.py
@@ -1,6 +1,7 @@
 import splendor_cards as cards
 import splendor_nobles as nobles
 import splendor_player as player
+import splendor_common as table
 import random
 
 red, green, blue, white, black, yellow = 'red', 'green', 'blue', 'white', 'black', 'yellow'
@@ -11,6 +12,7 @@ deck1 = cards.deckLVL1.copy()
 random.shuffle(deck1)
 
 tableCards = deck1[0:4]
+tableTokens = table.setTableTokens(2)
 
 
 player1 = player.player('Toby')
@@ -31,7 +33,7 @@ def setTableCards(deck):
     return tableCards
 
 
-def canPurchaseCard(player, card):
+def isCardPurchasePossible(player, card):
     cardRequirements = card.easyRequirements
     playerTokens = player.tokens
 
@@ -43,19 +45,23 @@ def canPurchaseCard(player, card):
             return False
     return True        
 
-def purchaseCard(player, card):
-    if canPurchaseCard(player, card):
+def purchaseCard(player, card, tableTokens):
+    if isCardPurchasePossible(player, card):
         for color in card.easyRequirements.keys():
             player.tokens[color] -= card.easyRequirements[color]
+            tableTokens[color] += card.easyRequirements[color]
         player.addCard(card)
-    return player
+    return player, tableTokens
 
-print(len(deck1))
-tableLv1Cards = setTableCards(deck1)
-print(len(deck1))
-print(tableLv1Cards)
-# print(player1.cards)
-# print(player1.tokens)
-# player1 = purchaseCard(player1, oneCard)
-# print(player1.cards)
-# print(player1.tokens)
+# print(len(deck1))
+# tableLv1Cards = setTableCards(deck1)
+# print(len(deck1))
+# print(tableLv1Cards)
+
+
+print(player1.cards)
+print(player1.tokens)
+player1, tableTokens = purchaseCard(player1, tableCards[0], tableTokens)
+print(player1.cards)
+print(player1.tokens)
+print(tableTokens)
