497a531921832a0cdd18bfbff7108f0eeabd529c
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2019-10-03 14:47:40 +0900

    add click and drag to test
    User can click a token and drag it away from the stack.
    The stack shows the updated quantity of tokens.

--- graphics_tests.py
+++ graphics_tests.py
@@ -7,22 +7,6 @@ from pygame.locals import *
 from splendor_gameUtils import *
 
 
-tableTokens = {
-    red:3,
-    green:3,
-    blue:3,
-    white:3,
-    black:3,
-    yellow:3
-}
-
-
-redTokenSurf = pygame.Surface((60,70), pygame.SRCALPHA)
-redTokenRect = redTokenSurf.get_rect()
-redTokenRect.topleft = (100,100)
-
-redTokenSurf.fill(NOCOLOUR)
-
 
 def drawTokens(asurface, tokens):
     redTokens = tokens[red]
@@ -31,11 +15,14 @@ def drawTokens(asurface, tokens):
     whiteTokens = tokens[white]
     blackTokens = tokens[black]
 
+    
+
 
     if redTokens:
+        redTokenSurf.fill(BLACK)     
         
         for token in range(redTokens):
-            baseYPos = 40
+            baseYPos = 50
             tokenYPos = baseYPos - (3*token)
             
             pygame.draw.circle(redTokenSurf, LIGHTRED, (30,tokenYPos), 30)
@@ -45,13 +32,17 @@ def drawTokens(asurface, tokens):
         asurface.blit(redTokenSurf, redTokenRect)
 
     if greenTokens:
+        greenTokenSurf.fill(NOCOLOUR)
         for token in range(greenTokens):
-            baseYPos = 100
+
+            baseYPos = 50
             tokenYPos = baseYPos - (3*token)
             
-            pygame.draw.circle(asurface, LIGHTGREEN, (200,tokenYPos), 30)
-            pygame.draw.circle(asurface, GREEN, (200,tokenYPos), 30, 1)
+            pygame.draw.circle(greenTokenSurf, LIGHTGREEN, (30,tokenYPos), 30)
+            pygame.draw.circle(greenTokenSurf, GREEN, (30,tokenYPos), 30, 1)
             # pygame.draw.circle(asurface, BLACK, (100,tokenYPos), 30, 2)
+        asurface.blit(greenTokenSurf, greenTokenRect)
+
 def drawTokenAtMouse(x,y, color, asurface):
     movingTokenSurf = pygame.Surface((60, 60), pygame.SRCALPHA)
     # movingTokensurf = movingTokenSurf.convert_alpha()
@@ -60,28 +51,59 @@ def drawTokenAtMouse(x,y, color, asurface):
     movingTokenRect.center = (x, y)
     if color == red:
         highlight = LIGHTRED
-        shade = RED    
+        shade = RED
+
+    elif color == green:
+        highlight = LIGHTGREEN
+        shade = GREEN    
 
     pygame.draw.circle(movingTokenSurf, highlight, (30,30), 30)
     pygame.draw.circle(movingTokenSurf, shade, (30,30), 30, 1)
     asurface.blit(movingTokenSurf, movingTokenRect)
 
 
-def getTokenClicked(x,y):
+def getTokenClicked(x,y, tokens):
     
     if redTokenRect.collidepoint(x, y):
         print('Clicked RED')
-        return red
+        tokens[red] -= 1
+        print(f'There are now {tokens[red]} red tokens left.')
+        return red, tokens
+    elif greenTokenRect.collidepoint(x, y):
+        print('Clicked GREEN')
+        tokens[green] -= 1
+        print(f'There are now {tokens[green]} green tokens left.')
+        return green, tokens
+    
     else:
-        return None
+        return None, tokens
 
 
 def main():
-    global redTokenSurf
+    global redTokenSurf, redTokenRect, greenTokenSurf, greenTokenRect
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('SPLENDOR')
+
+    tableTokens = {
+        red:6,
+        green:3,
+        blue:3,
+        white:3,
+        black:3,
+        yellow:3
+    }
+
+    redTokenSurf = pygame.Surface((60,80), pygame.SRCALPHA)
+    redTokenSurf.fill(NOCOLOUR)
+    redTokenRect = redTokenSurf.get_rect()
+    redTokenRect.topleft = (100,100)
+
+    greenTokenSurf = pygame.Surface((60,80), pygame.SRCALPHA)
+    greenTokenSurf.fill(NOCOLOUR)
+    greenTokenRect = greenTokenSurf.get_rect()
+    greenTokenRect.topleft = (200,100)     
 
     mouseDown = False
     tokenClicked = None
@@ -98,10 +120,11 @@ def main():
             if event.type == MOUSEMOTION:
                 mousex, mousey = event.pos
 
+
             if event.type == MOUSEBUTTONDOWN:
                 mouseDown = True
                 mousex, mousey = event.pos
-                tokenClicked = getTokenClicked(mousex, mousey)
+                tokenClicked, tableTokens = getTokenClicked(mousex, mousey, tableTokens)
                 
             if event.type == MOUSEBUTTONUP:
                 mouseDown = False
